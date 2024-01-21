d6dccbe7a70cd27d7b27facaf58da53867cb2d04
Olga Krukova <krukovaolga1@gmail.com>
2024-01-21 14:02:13 +0200

    feat:implement reading from console and writing text to the text.txt file according to 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const pathJoined = path.join(__dirname, 'text.txt');
+const streamWrite = fs.createWriteStream(pathJoined, {
+  encoding: 'utf-8',
+});
+streamWrite.on('error', (e) => {
+  console.log(`Something goes wrong: ${e.message}`);
+});
+const cli = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Input some text here or type `exit` / press `Ctrl + c` to finish: ',
+});
+//Invoke prompt
+cli.prompt();
+cli.setPrompt('Enter more text or type `exit` / press `Ctrl + c` to finish: ');
+cli
+  .on('line', (line) => {
+    const lineTrimmed = line.trim();
+    switch (lineTrimmed) {
+      case 'exit':
+        cli.close();
+        break;
+      default:
+        streamWrite.write(lineTrimmed + '\n');
+        cli.prompt();
+        break;
+    }
+  })
+  .on('close', () => {
+    streamWrite.end();
+    streamWrite.on('finish', () => {
+      console.log(`\nCheck the text in the ${pathJoined}`);
+    })
+});
+
